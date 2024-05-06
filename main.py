30a50743ec0a1c00143407ce0be29cf72ac335d9
Leonardo Andrade <leofmandrade@gmail.com>
2024-05-06 12:21:49 -0300

    a

--- main.py
+++ main.py
@@ -758,7 +758,9 @@ def main(code):
 if __name__ == "__main__":
     # entrada é "main.py [arquivo]" 
     file = open(sys.argv[1], "r")
+    #printar o input do arquivo recebido
     code = file.read()
+    print (code)
     file.close()
     #adicionar o header.asm no começo do saida.asm e o footer.asm no final
     
